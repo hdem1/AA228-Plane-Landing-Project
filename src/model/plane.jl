9cd51c9f292ca3ac80f3d43a04b327450900bf5f
Henry Demarest <46830987+hdem1@users.noreply.github.com>
2025-11-14 16:14:44 -0800

    Reward stuff

--- src/model/plane.jl
+++ src/model/plane.jl
@@ -7,10 +7,12 @@ struct Plane
     c_drag::Float64
     A_wing::Float64
     F_engine::Float64
+    max_landing_vx::Float64
+    max_landing_vy::Float64
 end
 
 function Plane(plane_name::String)
     filepath = "configs/vehicles/"*planet_name*".yaml"
     config = YAML.load_file(filepath, dicttype=Dict{String, Any})
-    return Plane(name, config["mass"], config["m_lift"], config["b_lift"], config["a_drag"],  config["c_drag"], config["A_wing"], config["F_engine"])
+    return Plane(name, config["mass"], config["m_lift"], config["b_lift"], config["a_drag"],  config["c_drag"], config["A_wing"], config["F_engine"], config["max_landing_vx"], config["max_landing_vy"])
 end
--- src/simulation/simulate.jl
+++ src/simulation/simulate.jl
@@ -8,11 +8,36 @@ function step(state::State, action::Action, sim_config::SimConfig)
     # Update airspeed:
     new_state = set_airspeeds(state2, run_config)
 
-    # Check for hitting ground:
-
     # Get reward
-    # reward = 
+    reward, terminate = get_reward_and_terminate(state, action, sim_config)
 
     # Return
-    return new_state #, reward, terminate
+    return new_state, reward, terminate
+end
+
+function get_reward_and_terminate(state::State, action::Action, sim_config::SimConfig) 
+    reward = 0.0
+    terminate = false;
+
+    # Got to ground:
+    if state.y <= 0
+        terminate = true;
+        if state.vy <= sim_config.plane.max_landing_vy #Crashed
+            reward -= 10000
+        else #Successful landing
+            reward += 5000
+            reward -= abs(state.vy) * 100 # Reward less landing impact
+            reward -= max(0, state.vx - sim_config.plane.max_landing_vx) * 20 # Punish landing with too much horizontal speed
+        end
+    end
+
+    # Small punishment for going around (x > width)
+    if state.y > 0 && state.x >= sim_config.scene_geometry.width
+        terminate = true
+        reward -= 1000
+    end 
+
+    # Punish significant state changes?
+
+    return reward, terminate
 end
